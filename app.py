38673a8e1b5fc371e8a12d4b58c97ecdff20b803
Naveen Kumar S <35146735+technofreak98@users.noreply.github.com>
2021-08-21 13:25:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,4 @@ def extract_details(file_path):
         return (str(e))
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
